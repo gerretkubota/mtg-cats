64cb161f48f05b0595e029012262a9caf51f5353
gerretkubota <gerretkubota@gmail.com>
2019-10-09 02:51:52 -0700

    Setting up basic frontend with React and being able to gather data via componentdidmount

---
+++ src/client/App.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      cats: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get('/images').then(result => {
+      const cats = result.data.map(info => info);
+
+      this.setState({ cats });
+    });
+  }
+
+  render() {
+    const { cats } = this.state;
+
+    return (
+      <div>
+        {cats.length ? (
+          <ul>
+            {cats.map(info => (
+              <li key={info.d}>{info.id}</li>
+            ))}
+          </ul>
+        ) : null}
+      </div>
+    );
+  }
+}
